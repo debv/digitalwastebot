e6694d9d55778c5f89279991a5166b07e70fd293
Deborah Venuti <deborahvenuti93@gmail.com>
2017-03-28 15:07:24 -0400

    Initial commit of script and files

---
+++ dwbot.py
@@ -0,0 +1,70 @@
+import sys
+import os
+import twitter
+import time
+import random
+from twilio.rest import TwilioRestClient
+from keys import *
+
+def main():
+
+    api = twitter.Api(
+        consumer_key,
+        consumer_secret,
+        access_key,
+        access_secret
+    )
+
+    client = TwilioRestClient(twil_sid, twil_auth)
+
+    tweet = 1
+    tweetedQuotes = 0
+    imagesPath = os.listdir('./images')
+    
+    # Put quotes into list for random tweeting
+    f = open('quotes/all.txt', 'r')
+    quotes = f.readlines()
+    f.close
+   
+    # Put images into list for random tweeting 
+    images = []
+    for image in imagesPath:
+        if(image != '.DS_Store'):
+            images.append('images/'+image)
+
+    # Keep tweeting while there are things to tweet
+    while(len(quotes) > 0 or len(images) > 0): 
+        # Tweet one image for every 10 quotes
+        if(tweetedQuotes < 10):
+            tweet = random.choice(quotes)
+            tweetIndex = quotes.index(tweet)
+            api.PostUpdate(tweet)
+            del quotes[tweetIndex]
+            tweetedQuotes += 1
+        elif(tweetedQuotes >= 10):
+            tweet = random.choice(images)
+            tweetIndex = images.index(tweet)
+            api.PostUpdate('',image)
+            del images[tweetIndex]
+            tweetedQuotes = 0
+
+        # Text me if there are 5 images or quotes remaining
+        if(len(quotes) == 5 or len(images) == 5):
+            stats = ("DigitalWasteBot is running low on content!\n"
+                + str(5) 
+                + " Quotes Remaining\n" 
+                + str(6) 
+                + " Images Remaining")
+            
+            message = client.messages.create(body=stats,
+                to="+13059679060",
+                from_="+17866614259")
+
+        # Only send out a tweet at 11am EST (1000 GMT) and 6pm EST (1800 GMT)
+        if(time.gmtime().tm_hour == 10):
+            time.sleep(25200) # Tweet again at 6pm
+        elif(time.gmtime().tm_hour == 18):
+            time.sleep(61200) # Tweet again at 11am
+
+if __name__ == '__main__':
+    main()
